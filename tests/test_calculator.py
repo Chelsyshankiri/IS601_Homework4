e7500b6656ac530366011538f2ad7b78826f2043
Chelsyshankiri <cs688@njit.edu>
2025-04-22 18:51:33 -0400

    Pylint changes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,20 +3,25 @@ import pytest
 from calculator import Calculator
 
 def test_add():
+    '''Tests the addition function with encapsulation'''
     result = Calculator.add(2,3)
     assert result == 5
 
 def test_subtract():
+    '''Tests the subtraction function with encapsulation'''
     assert Calculator.subtract(5,3) == 2
 
 
 def test_multiply():
+    '''Tests the multiplication function with encapsulation'''
     assert Calculator.multiply(4,5) == 20
 
 
 def test_divide():
+    '''Tests the division function with encapsulation'''
     assert Calculator.divide(10,2) == 5
 
 def test_dividebyzero():
+    '''Tests the division function in dividebyzero scenario with encapsulation'''
     with pytest.raises(ValueError):
         Calculator.divide(10,0)
