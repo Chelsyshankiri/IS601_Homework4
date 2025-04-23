211f44374c7fc721de95a26902597ff7207a8502
Chelsyshankiri <cs688@njit.edu>
2025-04-22 20:45:35 -0400

    Added faker update for random generation

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -7,20 +7,9 @@ import pytest
 from calculator.Calculation import Calculation
 from calculator.operations import add,subtract,multiply,divide
 
-
-@pytest.mark.parametrize("value1, value2, operation, expected",
-[
-    (Decimal('2'), Decimal('3'), add, Decimal('5')), # Addition : 2 + 3 = 5
-    (Decimal('5'), Decimal('3'), subtract, Decimal('2')), # Subtraction : 5 - 3 = 2
-    (Decimal('15'), Decimal('3'), divide, Decimal('5')), # Division : 15 / 3 = 5
-    (Decimal('4'), Decimal('3'), multiply, Decimal('12')), # Multiply : 4 * 3 = 12
-    (Decimal('3'), Decimal('3'), add, Decimal('6')) # Addition : 3 + 3 = 6
-]
-)
-
-def test_calculate(value1: Decimal, value2: Decimal, operation, expected):
+def test_calculate(a: Decimal, b: Decimal, operation, expected):
     ''' Test Calculation object computes correct results for the operations. '''
-    obj = Calculation(value1, value2, operation)
+    obj = Calculation(a, b, operation)
     assert obj.calculate() == expected, f"Operation {operation.__name__} has been failed!!"
 
 def test_repr():
